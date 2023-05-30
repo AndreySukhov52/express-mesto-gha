47008a2cba2f8502aacabe63a80aca55d9e7eb54
Andrey Sukhov <andrei11152@yandex.ru>
2023-05-30 22:40:16 +0300

    fix: исправили все

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const express = require('express');
 const mongoose = require('mongoose');
-//const { StatusCodes } = require('http-status-codes');
+const { errors } = require('celebrate');
+const cookieParser = require('cookie-parser');
+const rateLimit = require('express-rate-limit');
 const routes = require('./routes/index');
+const errorHandler = require('./middlewares/errorHandler');
+const { createUser, login } = require('./controllers/users');
+const { validationSignIn, validationSignUp } = require('./middlewares/validation');
 
 const app = express();
 
-/**  подключаемся к серверу mongo */
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, /**  за 15 минут */
+  max: 100, /** можно совершить максимум 100 запросов с одного IP */
+});
+/** подключаемся к серверу mongo */
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
 });
 
 app.use(express.json());
+app.use(cookieParser());
 const { PORT = 3000 } = process.env;
 
 /** подключаем мидлвары, роуты и всё остальное... */
-app.use((req, res, next) => {
-  req.user = {
-    _id: '644e581ce2abc84dce828420', /**  вставьте сюда _id созданного в предыдущем пункте пользователя */
-  };
 
-  next();
-});
+app.post('/signup', validationSignUp, createUser);
+app.post('/signin', validationSignIn, login);
+
+app.use(limiter);
 
 app.use(routes);
+
+app.use(errors());
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`app слушает порт: ${PORT}`);
