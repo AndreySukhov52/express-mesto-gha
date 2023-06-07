f66948899d9890713e83c9619a358bb9d9e0d1cd
Andrey Sukhov <andrei11152@yandex.ru>
2023-06-08 00:49:00 +0300

    fix: test3.14

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,12 @@ const cardRoutes = require('./cards');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/notFound-error');
 
+router.post('/signin', validationSignIn, login);
+router.post('/signup', validationSignUp, createUser);
+
 router.use('/users', auth, userRoutes);
 router.use('/cards', auth, cardRoutes);
-router.post('/signup', validationSignUp, createUser);
-router.post('/signin', validationSignIn, login);
+
 
 // router.use(('*', auth, (req, res, next) => {
 //   next(new NotFoundError('Запрашиваемый ресурс не найден'));
